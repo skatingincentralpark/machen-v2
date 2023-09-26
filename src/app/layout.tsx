70f6c6aa596ab49e1811d9b05af17b66f317434b
Charlie Zhao <charles.zhao5461@gmail.com>
2023-09-26 23:44:29 +0900

    Edit fontfamily

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import Header from "@/components/Header";
 import "@/styles/global.css";
-import { Inter } from "next/font/google";
+// import { Inter } from "next/font/google";
 import { NotesController } from "@/context/NotesContext";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
@@ -12,7 +12,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body
+      // className={inter.className}
+      >
         <NotesController>
           <Header />
           {children}
