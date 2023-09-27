79c697f721f30206741d5a0c3bcf634f61036d13
Charlie Zhao <charles.zhao5461@gmail.com>
2023-09-27 16:45:16 +0900

    Add some explanation to header menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@
 import styled from "@emotion/styled";
 import { useEffect, useState } from "react";
 import HeaderMenu from "@/components/HeaderMenu";
+import { media } from "@/lib/media-queries";
 
 const Header = () => {
   const [localeDateTime, setLocaleDateTime] = useState<string>("");
@@ -16,7 +17,7 @@ const Header = () => {
     <>
       <HeaderMain>
         <Logo>
-          Machen <TimeWrapper>{localeDateTime}</TimeWrapper>{" "}
+          <div>Machen</div> <TimeWrapper>{localeDateTime}</TimeWrapper>
         </Logo>
         <HeaderMenu />
       </HeaderMain>
@@ -44,7 +45,7 @@ const HeaderMain = styled.header`
   }
 `;
 const Logo = styled.div`
-  padding: 2rem;
+  padding: 2rem 0 2rem 2rem;
 `;
 
 const TimeWrapper = styled.div`
@@ -53,6 +54,11 @@ const TimeWrapper = styled.div`
 
   animation: fadeIn 2s ease-in-out;
 
+  max-width: 9rem;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+
   @keyframes fadeIn {
     0% {
       opacity: 0;
@@ -60,5 +66,9 @@ const TimeWrapper = styled.div`
     100% {
       opacity: 1;
     }
+  }
+
+  ${media.sm} {
+    max-width: 100%;
   }
 `;
--- src/components/HeaderMenu.tsx
+++ src/components/HeaderMenu.tsx
@@ -19,16 +19,40 @@ const HeaderMenu = () => {
       <PopoverTrigger asChild>
         <HeaderMenuButton>
           <Circle />
+          <InfoText>Click the circle for some information!</InfoText>
         </HeaderMenuButton>
       </PopoverTrigger>
       <Anchor />
       <PopoverPortal>
         <HeaderMenuContent>
-          <PopoverClose asChild>
-            <Button>Close</Button>
-          </PopoverClose>
+          <TextContainer>
+            <p>
+              This web app allows you to record daily notes. It uses{" "}
+              <a
+                href="https://lexical.dev/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Lexical
+              </a>
+              , as the rich text editor.
+            </p>
+            <p>You should be able to use the app with just your keyboard.</p>
+            <p>
+              <strong>Navigate</strong>: `tab` and `shift + tab`{" "}
+            </p>
+            <p>
+              <strong>Select</strong>: `space` or `enter`
+            </p>
+            <p>
+              In some cases you may need to use the arrow keys, such as
+              selecting text type in editor menu
+            </p>
+          </TextContainer>
           <ActionContainer>
-            <p>Set dummy notes for testing</p>
+            <PopoverClose asChild>
+              <Button>Close</Button>
+            </PopoverClose>
             <PopoverClose asChild>
               <HeaderButton
                 onClick={setDummyNotes}
@@ -46,9 +70,14 @@ const HeaderMenu = () => {
 
 export default HeaderMenu;
 
+const TextContainer = styled.div`
+  max-width: 20rem;
+  text-align: right;
+`;
 const HeaderMenuButton = styled.button`
   padding: 1.5rem 2rem;
   background-color: transparent;
+  position: relative;
 
   &:active > * {
     transform: scale(0.5);
@@ -63,17 +92,28 @@ const Anchor = styled(PopoverAnchor)`
 const Circle = styled.div`
   height: 1.5rem;
   width: 1.5rem;
-  background: black;
+  background-color: black;
   border-radius: 50%;
   transition: 0.5s;
+
+  animation: colorChange 5s forwards;
+
+  @keyframes colorChange {
+    0% {
+      background-color: darkorange;
+    }
+    100% {
+      background-color: black;
+    }
+  }
 `;
 const HeaderMenuContent = styled(PopoverContent)`
   width: 100vw;
-  height: 50vh;
+  height: auto;
   position: fixed;
   top: 0;
   left: 0;
-  background: #f2f2f2;
+  background-color: #f2f2f2;
   padding: 2rem;
   display: flex;
   flex-direction: column;
@@ -88,4 +128,25 @@ const ActionContainer = styled.div`
 `;
 const HeaderButton = styled(Button)`
   width: fit-content;
+`;
+const InfoText = styled.div`
+  position: absolute;
+  transform: translateY(-50%);
+  top: 50%;
+  right: 100%;
+  background-color: darkorange;
+  padding: 0.5rem;
+  border-radius: 0.5rem;
+  color: white;
+  width: 15rem;
+
+  animation: fadeOut 5s forwards;
+  @keyframes fadeOut {
+    0% {
+      opacity: 1;
+    }
+    100% {
+      opacity: 0;
+    }
+  }
 `;
